37f73a36ab90fbe8fb43f67fc9c69156a3e8758d
lwh <1020529941@qq.com>
2021-11-16 23:23:35 +0800

    fix:新增大数据导出

--- src/views/excel/Excel.vue
+++ src/views/excel/Excel.vue
@@ -33,8 +33,11 @@
         </div>
       </template>
     </div>
-
     <button @click="exportSeason">导出绩效信息</button>
+
+    <p><input type="number" v-model="rows" />行</p>
+    <p><input type="number" v-model="cols" />列</p>
+    <button @click="outPutBigData">导出自定义大小表格信息</button>
   </div>
 </template>
 
@@ -74,7 +77,9 @@ export default {
           { name: "小凌", score: 26 },
           { name: "盖伦", score: 34 }
         ]
-      ]
+      ],
+      rows: 100, // 行
+      cols: 100 // 列
     };
   },
   computed: {},
@@ -116,6 +121,41 @@ export default {
         datas,
         "部门季度绩效" + new Date().toLocaleDateString(),
         sheets
+      ); // 对应下载文件的名字
+    },
+    // 导出自定义数据
+    outPutBigData() {
+      // 引入文件
+      const { export_json_to_excel } = require("vendor/Export2Excel.js");
+      // 表头
+      let tHeader = [];
+      // table表格中对应的属性名
+      let filterVal = [];
+      // 需要导出的内容
+      let pushData = [];
+      // 自定义生成列
+      for (let i = 0; i < this.cols * 1; i++) {
+        tHeader.push(`第${i + 1}列数据`);
+        filterVal.push(i);
+      }
+      // 生成自定义数据
+      for (let j = 0; j < this.rows * 1; j++) {
+        let pushObj = {};
+        for (let i = 0; i < this.cols * 1; i++) {
+          // 这边为展示数据不一致性设置为随机数
+          pushObj[i] = Math.random();
+        }
+        // 插入一条数据
+        pushData.push(pushObj);
+      }
+      // ---至此模拟后端请求数据结束---
+
+      // 表格绑定数据转json
+      const data = this.formatJson(filterVal, pushData);
+      export_json_to_excel(
+        tHeader,
+        data,
+        "自定义导出数据" + new Date().toLocaleDateString()
       ); // 对应下载文件的名字
     },
     // 导出列表格式化数据的方法
