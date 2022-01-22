7d941853e1ce57cc3da92e72b90d52af431c6752
LingWeihao <1020529941@qq.com>
2022-01-22 09:36:06 +0800

    121

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', [
+            "feat", "upd", "del", "fix", "refactor", "test", "perf", "docs", "style", "revert"
+        ]],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never']
+
+    }
+};
